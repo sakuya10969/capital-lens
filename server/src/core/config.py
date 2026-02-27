d134cdf56918b4a1ce71d214aa9249ca6ae72f81
sakuya10969 <sakuya.f20010618@gmail.com>
2026-02-27 11:45:53 +0900

    fix: core

--- server/src/core/config.py
+++ server/src/core/config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,11 +8,7 @@ load_dotenv()
 class Settings:
     """Application settings loaded from environment variables."""
 
-    FRED_API_KEY: str = os.getenv("FRED_API_KEY", "")
-
-    # Timeout settings (seconds)
     YFINANCE_TIMEOUT: int = int(os.getenv("YFINANCE_TIMEOUT", "15"))
-    FRED_TIMEOUT: int = int(os.getenv("FRED_TIMEOUT", "10"))
     JPX_TIMEOUT: int = int(os.getenv("JPX_TIMEOUT", "15"))
 
 
